7f344b09c6df51028299469e6d64659622e171bd
wastella <william.a.stella@gmail.com>
2020-07-02 10:29:54 -0400

    Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Dragoon
+# Dragoon (Toy Programming Language)
+
+## About
+Dragoon is a toy programming language. It is statically typed and is heavilyinfluenced by Python. I tried my best not to do the same exact syntax but ... it is hard. 
+
---
+++ src/classes.py
@@ -0,0 +1,122 @@
+#
+# Creating object classes
+# By: William Stella
+#
+
+
+# The only reason that I have a variable class is, if I want to add built-in-functions that operate on the variables, then they can be methods in this class:
+class Variable:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+
+class Dragoon:
+    def __init__(self):
+
+        self.vars = {}
+
+        self.keywords = ["declare"]
+
+        self.operators = ["::"]
+
+        self.funcs = {"print" : "dragoon"}
+
+        self.line_number = 0
+
+    def add_var(self, var_name, var_value):
+
+        self.vars.update({var_name: var_value})
+
+    def print_func(self, message):
+        
+        while True:
+            
+            # If the argument is a string (In dragoon, this is denoted by the "" groupings):
+            if message[0] == '"' and message[-1] == '"':
+                print(str(message.replace('"', "")))
+
+                break
+
+            # If the argument is an int (In dragoon, this is denoted by the '' groupings):
+            elif message[0] == "'" and message[-1] == "'":
+                print(int(message.replace("'", "")))
+
+                break
+
+            # If the argument is a bool (In dragoon, this is denoted by the <> groupings):
+            elif message[0] == "<" and message[-1] == ">":
+                # Replacing the "<" char in message
+                partially_replaced_message = message.replace("<", "")
+
+                # Replacing the ">" char in message
+                fully_replaced_message = partially_replaced_message.replace(">", "")
+
+                print(fully_replaced_message)
+
+                break
+
+            # If the argument is not a string, int, or bool then it is a var:
+            else:
+                try:
+                    message = self.vars[message.strip("\n")]
+
+                except KeyError:
+                    print("[Error] Syntax Error: The argument: {} on line {} was invalid".format(message, self.line_number))
+                    break
+
+    def tokenize(self, string, type):
+        if type == "function":
+            token_list = string.split("{")
+
+            # Updating the token list
+            token_list[1] = token_list[1].replace("}", "")
+            token_list[0] = token_list[0].split(".")[1]
+
+        elif type == "variable":
+            token_list = string.split()
+            if token_list[-2] != "::":
+                token_list[-2] = token_list[-2] + " " + token_list[-1]
+                token_list.pop(-1)
+            
+
+        elif type == "test":
+            token_list = string.split()
+
+        return token_list
+
+    def parse(self, contents):
+
+        for line in contents:
+
+            self.line_number += 1
+            
+            # Testing for what the file contents looks like so we can decide whether we should tokenize using the "variable" arg or the "function" arg.
+            example_tokens = self.tokenize(line, "test")
+
+
+            if line == "\n" or line[0] == ">":
+                continue
+
+            # If they are declaring a var on the first line:
+            elif example_tokens[0] == "define":
+
+                tokens = self.tokenize(line, "variable")
+                
+                # Creating a new variable:
+                new_var = Variable(tokens[1], tokens[3])
+                self.vars.update({new_var.name : new_var.value})
+
+            # If it is a function usage:
+            elif len(example_tokens) == 1:
+
+                tokens = self.tokenize(line, "function")
+                self.print_func(tokens[1])
+
+            else:
+                print("[Error] Syntax Error: Syntax on line {} was unrecognizable.".format(self.line_number))
+                break
+
+with open("example.dg", "r") as contents_file:
+    new_file = Dragoon()
+    new_file.parse(contents_file)
